14a2b1510a62a59684786716102fd53877db247a
zke3344 <1433861798@qq.com>
2023-04-17 17:05:32 +0800

    菜单添加与查询

--- api/enter.go
+++ api/enter.go
@@ -3,13 +3,15 @@ package api
 import (
 	"gvb_server/api/advert_api"
 	"gvb_server/api/images_api"
+	"gvb_server/api/menu_api"
 	"gvb_server/api/settings_api"
 )
 
 type ApiGroup struct {
 	SettingsApi settings_api.SettingsApi
-	ImagesApi images_api.ImagesApi
-	AdvertApi advert_api.AdvertApi
+	ImagesApi   images_api.ImagesApi
+	AdvertApi   advert_api.AdvertApi
+	MenuApi     menu_api.MenuApi
 }
 
 var ApiGroupApp = new(ApiGroup)
--- routers/images_router.go
+++ routers/images_router.go
@@ -2,10 +2,11 @@ package routers
 
 import "gvb_server/api"
 
-func (router RouterGroup) ImagesRouter()  {
+func (router RouterGroup) ImagesRouter() {
 	app := api.ApiGroupApp.ImagesApi
 	router.POST("images", app.ImageUploadView)
 	router.GET("images", app.ImageListView)
-	router.DELETE("images",app.ImageRemoveView)
-	router.PUT("images",app.ImageUpdateView)
+	router.GET("images_names", app.ImageNameListView)
+	router.DELETE("images", app.ImageRemoveView)
+	router.PUT("images", app.ImageUpdateView)
 }
