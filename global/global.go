b833d915633204908321f830bd6c9f3e061a50cf
zke3344 <1433861798@qq.com>
2023-04-19 08:44:54 +0800

    jwt用户注销

--- global/global.go
+++ global/global.go
@@ -1,6 +1,7 @@
 package global
 
 import (
+	"github.com/go-redis/redis"
 	"github.com/sirupsen/logrus"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -8,9 +9,9 @@ import (
 )
 
 var (
-	Config *config.Config
-	DB *gorm.DB
-	Log *logrus.Logger
+	Config   *config.Config
+	DB       *gorm.DB
+	Log      *logrus.Logger
 	MysqlLog logger.Interface
+	Redis    *redis.Client
 )
-
